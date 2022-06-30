f2ff32377f5d20dc837529d7e8c303f3b4e0566f
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-06-30 22:29:16 +0300

    move `ready` event to seperate file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,18 @@
 import {
-    Client, Constants, GuildChannelManager, TextChannel
+    Client, Constants, TextChannel
 } from 'discord.js';
 import { readdirSync } from 'node:fs';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import * as config from "../bot.config.json" assert { type: "json" };
-import * as guildIds from "../values/PAP/IDs.json" assert { type: "json" };
 import GenericEvent from './Events/GenericEvent';
 
 export let bugsChannel: TextChannel;
 export let logsChannel: TextChannel;
 
-if (!process.env.NODE_ENV || process.env.NODE_ENV === "development")
-    (await import('dotenv'))
-        .config({ path: (await import('find-config')).read('.env') })  //load env variables
-
+if (process.env.NODE_ENV !== "production") {
+    const dotenv = await import("dotenv");
+    dotenv.config({ debug: true });
+}
 console.log(`deployed in "${process.env.NODE_ENV}" mode\n`);
 
 export const PAP = new Client({
@@ -42,27 +40,9 @@ export const PAP = new Client({
     }
 });
 
-
 await PAP.login(process.env.BOT_TOKEN)
     .then(r => console.log(`logged in `))
     .catch(err => console.log(`ERROR ON LOGIN: \n${err}`));
-
-PAP.on('ready', async () => {
-    try {
-        PAP.user.setActivity('over you', { type: 'WATCHING' });
-        const PAPGuildChannels: GuildChannelManager = (await PAP.guilds.cache.get(config.guildID).fetch()).channels;
-        const initLogs = PAPGuildChannels.cache.get(guildIds.channels.init_logs) as TextChannel;
-        bugsChannel = PAPGuildChannels.cache.get(guildIds.channels.bugs) as TextChannel;
-        logsChannel = PAPGuildChannels.cache.get(guildIds.channels.logs) as TextChannel;
-        console.log('smooth init');
-
-    } catch (err) {
-        console.log('READY ERROR');
-        console.log(err);
-    }
-
-    console.log(`___ Initiated ___`);
-});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
